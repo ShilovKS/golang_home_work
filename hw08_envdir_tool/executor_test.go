21874e4d19dd03f33acf2405a44b8104ccda5151
ShilovKS <55180451+shilovks@users.noreply.github.com>
2025-02-10 23:41:20 +0300

    HW8 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,34 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("simple test", func(t *testing.T) {
+		err := os.Setenv("FAKEENV", "fakeValue")
+		if err != nil {
+			t.Errorf("error setting variable: %v", err)
+		}
+
+		var shell, flag string
+		if runtime.GOOS == "windows" {
+			shell = "cmd"
+			flag = "/c"
+		} else {
+			shell = "sh"
+			flag = "-c"
+		}
+
+		returnCode := RunCmd([]string{shell, flag, ""}, Environment{
+			"FAKEENV": EnvValue{"fakeValue", false},
+		})
+
+		require.Equal(t, os.Getenv("FAKEENV"), "fakeValue")
+		require.Equal(t, 0, returnCode)
+	})
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s <env-dir> <command> [args...]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		fmt.Fprintln(os.Stderr, "not enough arguments")
+		os.Exit(1)
+	}
+	envDir := args[0]
+	commandArgs := args[1:]
+	env, err := ReadDir(envDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading environment directory: %v\n", err)
+		os.Exit(1)
+	}
+	exitCode := RunCmd(commandArgs, env)
+	os.Exit(exitCode)
 }
